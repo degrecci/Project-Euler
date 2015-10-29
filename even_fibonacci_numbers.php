638b0f272918befd34662ebc3755442c949ce795
Henrique Degrecci <rickdegrecci@hotmail.com>
2015-10-29 09:05:24 -0200

    Even Fibonacci numbers
    Concluido

---
+++ even_fibonacci_numbers.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "<b>Even Fibonacci numbers:</b> ";
+
+$a = 1;
+$b = 2;
+$x = 0;
+$soma = 2;
+
+while ($x <= 4000000){
+
+  echo "$a, ";
+
+  $x = $a + $b;
+  $a = $b;
+  $b = $x;
+
+    if ($x % 2 == 0) {
+      $soma = $soma + $x;
+    }
+}
+    echo "<br><b>Soma de tudo:</b> $soma";
+?>
