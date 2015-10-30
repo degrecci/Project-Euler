ffa0122ef9c542c3af88f559d0794dfa7487f892
DEGRECCI <rickdegrecci@hotmail.com>
2015-10-30 10:52:22 -0200

    Versao resumida Sum square difference

--- sum_square_difference_resum.java
+++ sum_square_difference_resum.java
@@ -6,19 +6,15 @@ public static void main(String[] args) {
 
   Scanner leia = new Scanner(System.in);
 
-  int res1 = 0, res2 = 0;
+    int res1 = 0, res2 = 0;
 
     for(int x = 0; x <= 100; x++){
       res1 += x * x;
-
     }
-
     for(int y = 0; y <= 100; y++){
-
       res2 += y;
     }
-      res2 *= res2;
-
+    res2 *= res2;
     System.out.println("Square of the sum:  "+ res2);
     System.out.println("Sum os the squares: "+ res1);
     System.out.println("-----------------------------");
