76510357ae4e8cfa8aed240f871d3992201bc4be
DEGRECCI <rickdegrecci@hotmail.com>
2015-10-30 09:50:39 -0200

    Sum square difference

---
+++ Sumsquare.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Sumsquare{
+public static void main(String[] args) {
+
+  Scanner leia = new Scanner(System.in);
+
+  int[] result = new int [100];
+  int i = 0, soma = 0, a = 0;
+
+    for(int x = 0; x <= 100; x++){
+      result[i] = x * x;
+
+      soma = result[i] + soma;
+    }
+
+    for(int y = 0; y <= 100; y++){
+
+      a = a + y;
+    }
+
+    int resultadofinal = a*a - soma;
+
+    System.out.println("Square of the sum:  "+ a*a);
+    System.out.println("Sum os the squares: "+ soma);
+    System.out.println("-----------------------------");
+    System.out.println("Difference between: "+ resultadofinal);
+  }
+}
