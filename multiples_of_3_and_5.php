a523c098efadc849733eeb280ffee6d55358ea73
DEGRECCI <rickdegrecci@hotmail.com>
2015-10-30 11:04:00 -0200

    Multiples of 3 and 5

--- multiples_of_3_and_5.php
+++ multiples_of_3_and_5.php
@@ -7,22 +7,22 @@
   </head>
   <body>
 	<?php
-	
+
 	for($x = 1; $x < 1000;$x++){
 		if($x % 3 == 0){
-			$b = $b + $x;
+			$b += $x;
 		}
 		else if($x % 5 == 0){
-			$a = $a + $x;
+			$a += + $x;
 		}
-		
+
 	}
-	
+
 	echo "<p>Multiplos de 3: $b</p>";
 	echo "<p>Multiplos de 5: $a</p>";
-	echo "<p>Resultado: $b + $a</p>";
+	echo "<p>Resultado: </p>". ($b + $a);
 
-	
+
 	?>
   </body>
 
