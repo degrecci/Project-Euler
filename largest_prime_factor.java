f78d2292e5842c999a476a48b75249c754cb6bef
DEGRECCI <rickdegrecci@hotmail.com>
2015-10-30 13:56:23 -0200

    Largest Prima Factor

---
+++ largest_prime_factor.java
@@ -0,0 +1,20 @@
+public class largest_prime_factor{
+  
+  public static void main(String[] args) {
+
+    int x = 1;
+    long y = 600851475143L;
+    long fim = 600851475143L;
+
+    while(x <= fim){
+
+      x += 1;
+      if(y % x == 0){
+
+        System.out.println(x);
+        fim = fim/x;
+        }
+    }
+    System.out.println("Resultado: "+ x);
+  }
+}
