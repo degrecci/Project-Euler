b61093419fc362f01db64d83de1f4c28a3627922
DEGRECCI <rickdegrecci@hotmail.com>
2015-11-19 13:42:08 -0200

    Adicionando Versão Resumida

---
+++ 04-largest_palindrome_product/largest_palindrome_product_resum.php
@@ -0,0 +1,21 @@
+<?php
+$x1=100;
+$x2=100;
+
+while ($x1 < 999) {
+  $result = $x1 * $x2;
+  $x1++;
+  while ($x2 <= 999)
+  {
+    $result = $x1 * $x2;
+    $x2++;
+    if($result == strrev($result))
+    {
+      $r[] = $result;
+    }
+  }
+$x2 = 100;
+}
+
+echo "Largest Palindrome Product: <b>". max($r); # Execution time 40.994345 s
+?>
