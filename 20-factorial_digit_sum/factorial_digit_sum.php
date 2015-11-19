309894d0c963c49096252eb0263f21d43b9bc81f
DEGRECCI <rickdegrecci@hotmail.com>
2015-11-19 10:20:59 -0200

    Exercicio 20 - Concluído

--- 20-factorial_digit_sum/factorial_digit_sum.php
+++ 20-factorial_digit_sum/factorial_digit_sum.php
@@ -3,7 +3,7 @@ $resultado = 1;
 
 for($i = 2; $i <= 100; $i++){
     $resultado=bcmul($resultado, $i);
-    # bcmul - multiplica dois números de precisão arbitrária
+    # bcmul - multiplica dois numeros de precisao arbitraria
 }
 echo "Soma dos numeros do Fatorial de 100!: <b>" . array_sum(str_split($resultado));
 # str_split converte $resultado em um arra e array_sum soma o mesmo.
