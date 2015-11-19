89f1201129a9f11287b544f6c28a01c9b3df3b8b
DEGRECCI <rickdegrecci@hotmail.com>
2015-11-19 10:10:43 -0200

    Exercicio 01 - Concluído

--- 01-multiples_of_3_and_5/multiples_of_3_and_5.php
+++ 01-multiples_of_3_and_5/multiples_of_3_and_5.php
@@ -1,29 +1,13 @@
-<html>
-
-  <head>
-  <title>Titulo</title>
-    <meta charset="UTF-8">
-
-  </head>
-  <body>
-	<?php
-
-	for($x = 1; $x < 1000;$x++){
+<?php
+  for($x = 1; $x < 1000;$x++){
 		if($x % 3 == 0){
 			$b += $x;
 		}
 		else if($x % 5 == 0){
 			$a += $x;
 		}
-
 	}
-
 	echo "<p>Multiplos de 3: $b</p>";
 	echo "<p>Multiplos de 5: $a</p>";
 	echo "<p>Resultado: </p>". ($b + $a);
-
-
-	?>
-  </body>
-
-<html>
+?>
