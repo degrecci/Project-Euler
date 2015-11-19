c42bae3755c523eb23e978995384bb68cf5971d5
DEGRECCI <rickdegrecci@hotmail.com>
2015-11-19 10:07:05 -0200

    Exercicio 20 - Conluído

--- 20-factorial_digit_sum/factorial_digit_sum.php
+++ 20-factorial_digit_sum/factorial_digit_sum.php
@@ -1,9 +1,7 @@
 <?php
-
 $resultado = 1;
-$fatorial = 100;
 
-for($i = 2; $i <= $fatorial; $i++){
+for($i = 2; $i <= 100; $i++){
     $resultado=bcmul($resultado, $i);
     # bcmul - multiplica dois números de precisão arbitrária
 }
