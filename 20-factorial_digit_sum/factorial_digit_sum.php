42c84e29108ae4a01ff73e8eac8b99964acdc372
DEGRECCI <rickdegrecci@hotmail.com>
2015-11-19 08:07:40 -0200

    descrição

--- 20-factorial_digit_sum/factorial_digit_sum.php
+++ 20-factorial_digit_sum/factorial_digit_sum.php
@@ -7,6 +7,6 @@ for($i = 2; $i <= $fatorial; $i++){
     $resultado=bcmul($resultado, $i);
     # bcmul - multiplica dois números de precisão arbitrária
 }
-echo array_sum(str_split($resultado));
+echo "Soma dos numeros do Fatorial de 100!: <b>" . array_sum(str_split($resultado));
 # str_split converte $resultado em um arra e array_sum soma o mesmo.
 ?>
